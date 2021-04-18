dc56015e9cfcff3c39719fec134df953e8c3d10f
Arnau Diaz <diazarnau0@gmail.com>
2021-04-18 21:32:36 +0200

    rlist

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+mod cli;
+mod tasks;
+
+use structopt::StructOpt;
+
+use cli::{Action::*, CommandLineArgs};
+use tasks::Task;
+
+fn main() {
+    let CommandLineArgs {
+        action,
+        journal_file,
+    } = CommandLineArgs::from_args();
+
+    let journal_file = journal_file.expect("Failed to find journal file");
+
+    match action {
+        Add { text } => tasks::add_task(journal_file, Task::new(text)),
+        List => tasks::list_tasks(journal_file),
+        Done { position } => tasks::complete_task(journal_file, position),
+    }
+    .expect("Failed to perform action")
+}
